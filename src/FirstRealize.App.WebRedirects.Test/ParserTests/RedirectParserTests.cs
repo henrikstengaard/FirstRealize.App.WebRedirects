bbfbd6564e17cb73a01c2c40dc048bb8132033a7
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2018-09-18 11:58:41 +0200

    Added redirect parser tests

---
+++ src/FirstRealize.App.WebRedirects.Test/ParserTests/RedirectParserTests.cs
@@ -0,0 +1,35 @@
+﻿using FirstRealize.App.WebRedirects.Core.Configuration;
+using FirstRealize.App.WebRedirects.Core.Parsers;
+using NUnit.Framework;
+using System;
+
+namespace FirstRealize.App.WebRedirects.Test.ParserTests
+{
+    [TestFixture]
+    public class RedirectParserTests
+    {
+        [Test]
+        public void CanParseRedirect()
+        {
+            var redirectParser = new RedirectParser(
+                new Configuration
+                {
+                    DefaultOldUrl = TestData.DefaultHost,
+                    DefaultNewUrl = TestData.DefaultHost
+                },
+                new UrlParser());
+
+            var redirect = redirectParser.ParseRedirect(
+                "/old-url/#anchor",
+                "/new-url/#anchor");
+
+            Assert.IsNotNull(redirect);
+            Assert.AreEqual(
+                "http://www.test.local/old-url",
+                redirect.OldUrl.Parsed.AbsoluteUri);
+            Assert.AreEqual(
+                "http://www.test.local/new-url#anchor",
+                redirect.NewUrl.Parsed.AbsoluteUri);
+        }
+    }
+}
