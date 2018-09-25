6f95a04feb2f41eacf68c85ac574875455ad4fdb
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2018-09-25 11:02:37 +0200

    Fixed http client to use web request instead of http web request

--- src/FirstRealize.App.WebRedirects.Core/Clients/HttpClient.cs
+++ src/FirstRealize.App.WebRedirects.Core/Clients/HttpClient.cs
@@ -16,9 +16,16 @@ namespace FirstRealize.App.WebRedirects.Core.Clients
         public HttpResponse Get(
             string url)
         {
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            request.UserAgent = "WebRedirects Crawler";
-            request.AllowAutoRedirect = false;
+            var request = WebRequest.Create(url);
+            request.Method = "GET";
+
+            var httpWebRequest = request as HttpWebRequest;
+            if (httpWebRequest != null)
+            {
+                httpWebRequest.Accept = "*/*";
+                httpWebRequest.UserAgent = "WebRedirects Crawler";
+                httpWebRequest.AllowAutoRedirect = false;
+            }
 
             var httpResponse = new HttpResponse();
 
