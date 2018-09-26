8f575818e792090bef99f20972e595de2fc358ec
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2018-09-26 15:28:18 +0200

    Optimized regex use in url helper

--- src/FirstRealize.App.WebRedirects.Core/Helpers/UrlHelper.cs
+++ src/FirstRealize.App.WebRedirects.Core/Helpers/UrlHelper.cs
@@ -3,62 +3,75 @@ using FirstRealize.App.WebRedirects.Core.Formatters;
 using FirstRealize.App.WebRedirects.Core.Models.Urls;
 using FirstRealize.App.WebRedirects.Core.Parsers;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace FirstRealize.App.WebRedirects.Core.Helpers
 {
-	public class UrlHelper : IUrlHelper
+    public class UrlHelper : IUrlHelper
     {
         private readonly IConfiguration _configuration;
-		private readonly IUrlParser _urlParser;
-		private readonly IUrlFormatter _urlFormatter;
+        private readonly IUrlParser _urlParser;
+        private readonly IUrlFormatter _urlFormatter;
+
+        private readonly Regex _schemeRegex;
+        private readonly IList<Regex> _forceHttpHostRegexs;
+
 
         public UrlHelper(
             IConfiguration configuration,
-			IUrlParser urlParser,
-			IUrlFormatter urlFormatter)
+            IUrlParser urlParser,
+            IUrlFormatter urlFormatter)
         {
             _configuration = configuration;
-			_urlParser = urlParser;
-			_urlFormatter = urlFormatter;
+            _urlParser = urlParser;
+            _urlFormatter = urlFormatter;
+
+            _schemeRegex = new Regex(
+                    "^https?://",
+                    RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            _forceHttpHostRegexs = _configuration.ForceHttpHostPatterns
+                .Select(x => new Regex(
+                    x, RegexOptions.IgnoreCase | RegexOptions.Compiled))
+                .ToList();
         }
 
-		public string Combine(
-			string url1,
-			string url2)
-		{
-			var url1Parsed = _urlParser.Parse(url1);
+        public string Combine(
+            string url1,
+            string url2)
+        {
+            var url1Parsed = _urlParser.Parse(url1);
 
-			if (!url1Parsed.IsValid)
-			{
-				throw new UriFormatException(
-					string.Format(
-						"Url '{0}' format is not valid",
-						url1));
-			}
+            if (!url1Parsed.IsValid)
+            {
+                throw new UriFormatException(
+                    string.Format(
+                        "Url '{0}' format is not valid",
+                        url1));
+            }
 
-			var url2Parsed = _urlParser.Parse(url2);
+            var url2Parsed = _urlParser.Parse(url2);
 
-			if (!url2Parsed.IsValid)
-			{
-				throw new UriFormatException(
-					string.Format(
-						"Url '{0}' format is not valid",
-						url2));
-			}
+            if (!url2Parsed.IsValid)
+            {
+                throw new UriFormatException(
+                    string.Format(
+                        "Url '{0}' format is not valid",
+                        url2));
+            }
 
-			return _urlFormatter.Format(
-				new ParsedUrl
-				{
-					Scheme = url1Parsed.Scheme,
-					Host = url1Parsed.Host,
-					Port = url1Parsed.Port,
-					PathAndQuery = url2Parsed.PathAndQuery
-				});
-		}
+            return _urlFormatter.Format(
+                new ParsedUrl
+                {
+                    Scheme = url1Parsed.Scheme,
+                    Host = url1Parsed.Host,
+                    Port = url1Parsed.Port,
+                    PathAndQuery = url2Parsed.PathAndQuery
+                });
+        }
 
-		public bool IsHttpsRedirect(
+        public bool IsHttpsRedirect(
             string oldUrl,
             string newUrl)
         {
@@ -68,78 +81,47 @@ namespace FirstRealize.App.WebRedirects.Core.Helpers
                 return false;
             }
 
-            var oldUrlHttpsScheme = Regex.Replace(
-                oldUrl,
-                "^https?://",
-                "https://",
-                RegexOptions.IgnoreCase | RegexOptions.Compiled).ToLower();
-            return oldUrlHttpsScheme.Equals(
-                newUrl);
+            var oldUrlHttpsScheme =
+                _schemeRegex.Replace(oldUrl, "https://");
+
+            return oldUrlHttpsScheme.Equals(newUrl);
         }
 
         public string FormatUrl(
             string url)
         {
-			if (string.IsNullOrWhiteSpace(url) ||
-				!Regex.IsMatch(
-					url,
-					"^https?://",
-					RegexOptions.IgnoreCase | RegexOptions.Compiled))
-			{
-				return url;
-			}
+            if (string.IsNullOrWhiteSpace(url) ||
+                !_schemeRegex.IsMatch(url))
+            {
+                return url;
+            }
 
-			var uri = new Uri(url);
+            var uri = new Uri(url);
 
             var forceHttpUrlPatternMatch =
-				uri != null &&
-                _configuration.ForceHttpHostPatterns.Any(x => Regex.IsMatch(
-					uri.DnsSafeHost,
-                    x, RegexOptions.IgnoreCase | RegexOptions.Compiled) || Regex.IsMatch(
-					uri.Host,
-					x, RegexOptions.IgnoreCase | RegexOptions.Compiled));
+                _forceHttpHostRegexs.Any(x => x.IsMatch(uri.DnsSafeHost)) ||
+                _forceHttpHostRegexs.Any(x => x.IsMatch(uri.Host));
 
             return forceHttpUrlPatternMatch
-                ? Regex.Replace(
-                    url,
-                    "^https?://",
-                    "http://",
-                    RegexOptions.IgnoreCase | RegexOptions.Compiled)
+                ? _schemeRegex.Replace(url, "http://")
                 : url;
         }
 
-   //     public bool AreIdentical(
-   //         IUrl url1,
-   //         IUrl url2)
-   //     {
-   //         if (url1 == null ||
-   //             !url1.IsValid ||
-   //             url2 == null ||
-   //             !url2.IsValid)
-   //         {
-   //             return false;
-   //         }
+        public bool AreIdentical(
+            string url1,
+            string url2)
+        {
+            if (string.IsNullOrWhiteSpace(url1) ||
+                string.IsNullOrWhiteSpace(url2))
+            {
+                return false;
+            }
 
-			//return AreIdentical(
-			//	url1.Parsed.AbsoluteUri,
-			//	url2.Parsed.AbsoluteUri);
-   //     }
+            var url1Formatted = FormatUrl(url1);
+            var url2Formatted = FormatUrl(url2);
 
-		public bool AreIdentical(
-			string url1,
-			string url2)
-		{
-			if (string.IsNullOrWhiteSpace(url1) ||
-				string.IsNullOrWhiteSpace(url2))
-			{
-				return false;
-			}
-
-			var url1Formatted = FormatUrl(url1);
-			var url2Formatted = FormatUrl(url2);
-
-			return url1Formatted.Equals(
-				url2Formatted);
-		}
-	}
+            return url1Formatted.Equals(
+                url2Formatted);
+        }
+    }
 }
