feb5a4c34ef4812742d6d6305c27b283c1b11a39
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2018-10-08 21:50:39 +0200

    Optimized redirect tests

--- src/FirstRealize.App.WebRedirects.Test/ModelTests/RedirectTests.cs
+++ src/FirstRealize.App.WebRedirects.Test/ModelTests/RedirectTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using FirstRealize.App.WebRedirects.Core.Models.Redirects;
+using NUnit.Framework;
 using System.Linq;
 
 namespace FirstRealize.App.WebRedirects.Test.ModelTests
@@ -6,6 +7,62 @@ namespace FirstRealize.App.WebRedirects.Test.ModelTests
     [TestFixture]
     public class RedirectTests
     {
+        [Test]
+        public void SortInvalidParsedRedirects()
+        {
+            var parsedRedirects = TestData.TestData.GetParsedRedirects(
+                new[]
+                {
+                    new Redirect
+                    {
+                        OldUrl = string.Empty,
+                        NewUrl = string.Empty
+                    },
+                    new Redirect
+                    {
+                        OldUrl = string.Empty,
+                        NewUrl = "/url1"
+                    },
+                    new Redirect
+                    {
+                        OldUrl = "/url1",
+                        NewUrl = "/url2"
+                    }
+                })
+                .ToList();
+
+            parsedRedirects.Sort();
+
+            Assert.AreEqual(3, parsedRedirects.Count);
+            Assert.AreEqual(
+                false,
+                parsedRedirects[0].IsValid);
+            Assert.AreEqual(
+                string.Empty,
+                parsedRedirects[0].OldUrl.Raw);
+            Assert.AreEqual(
+                "/url1",
+                parsedRedirects[0].NewUrl.Raw);
+            Assert.AreEqual(
+                false,
+                parsedRedirects[1].IsValid);
+            Assert.AreEqual(
+                string.Empty,
+                parsedRedirects[1].OldUrl.Raw);
+            Assert.AreEqual(
+                string.Empty,
+                parsedRedirects[1].NewUrl.Raw);
+            Assert.AreEqual(
+                true,
+                parsedRedirects[2].IsValid);
+            Assert.AreEqual(
+                "http://www.test.local/url1",
+                parsedRedirects[2].OldUrl.Formatted);
+            Assert.AreEqual(
+                "http://www.test.local/url2",
+                parsedRedirects[2].NewUrl.Formatted);
+        }
+
         [Test]
         public void SortRedirectsInAscendingOrder()
         {
